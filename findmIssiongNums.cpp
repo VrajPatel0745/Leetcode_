acd799019d6aeb4367fadf20debdc3178bc053c6
VrajPatel0745 <vrajpatel074518@gmail.com>
2025-07-09 22:10:00 +0530

    Add initial implementation of missing number solution and VSCode settings

---
+++ findmIssiongNums.cpp
@@ -0,0 +1,24 @@
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+       int n = nums.size();
+        int expected_sum = n * (n + 1) / 2;
+        int actual_sum = 0;
+        for(int num : nums) {
+            actual_sum += num;
+        }
+        return expected_sum - actual_sum;
+    }
+    
+};
+int main() {
+    Solution solution;
+    vector<int> nums = {3, 0, 1};
+    cout << "Missing number: " << solution.missingNumber(nums) << endl; // Output: 2
+    return 0;
+}
