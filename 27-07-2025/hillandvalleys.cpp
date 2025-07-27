f05e4a3304ff5d53df2ad5954352db327d84a1ef
VrajPatel0745 <vrajpatel074518@gmail.com>
2025-07-27 18:39:00 +0530

    Add implementation of countHillValley function to count hill and valley points in an array

---
+++ 27-07-2025/hillandvalleys.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            
+            if (nums[i] == nums[i - 1]) continue;
+
+            int left = i - 1;
+            while (left >= 0 && nums[left] == nums[i]) {
+                left--;
+            }
+
+            int right = i + 1;
+            while (right < n && nums[right] == nums[i]) {
+                right++;
+            }
+            if (left >= 0 && right < n) {
+                if (nums[i] > nums[left] && nums[i] > nums[right]) {
+                    count++; 
+                } else if (nums[i] < nums[left] && nums[i] < nums[right]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
+int main() {
+    Solution solution;
+    vector<int> nums = {2, 4, 1, 1, 6, 5};
+    cout << "Number of hill and valley points: " << solution.countHillValley(nums) << endl;
+    return 0;
+}
